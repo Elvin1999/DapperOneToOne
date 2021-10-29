04914ed3a5de4009eb08933a0dd1b89f2e9f2453
Elvin Camalzade <e.camalzade@itstep.edu.az>
2021-10-29 15:49:07 +0400

    Add project files.

---
+++ DapperExample2/Entities/Capital.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DapperExample2.Entities
+{
+   public class Capital
+    {
+        public int CapitalId { get; set; }
+        public string Name { get; set; }
+        public int CountryId { get; set; }
+        public Country Country { get; set; }
+    }
+}
